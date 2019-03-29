666a51572a16face5c2a382ba76b07965b191a24
Paul Martin <communty.hivemind@gmail.com>
2019-03-29 16:21:37 +0000

    Factor out Bookmark.connection method and tidy up

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,42 +2,32 @@ require 'pg'
 
 class Bookmark
 
-  def self.all
+  def self.connection
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    result = connection.exec( "SELECT * FROM bookmarks")
-    # result.map { |bookmark| { :url => bookmark["url"], :title => bookmark["title"] } }
+    connection
+  end
+
+  def self.all
+    result = connection.exec("SELECT * FROM bookmarks")
     result.map do |bookmark|
-      self.new(id: bookmark["id"],
-               title: bookmark["title"],
-               url: bookmark["url"])
+      new(id: bookmark["id"], title: bookmark["title"], url: bookmark["url"])
     end
   end
 
   def self.add(url:, title:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
+    result = connection.exec("INSERT INTO bookmarks (url, title)
+                              VALUES ('#{url}', '#{title}')
+                              RETURNING id, title, url;")
 
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES ('#{url}', '#{title}') RETURNING id, title, url;")
-    Bookmark.new(id: result[0]['id'],
-                 title: result[0]['title'], 
-                 url: result[0]['url'] )
+    new(id: result[0]["id"], title: result[0]["title"], url: result[0]["url"])
   end
 
   def self.delete(id)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
     connection.exec("DELETE FROM bookmarks WHERE id = #{id};")
-
   end
 
   attr_reader :id, :title, :url
